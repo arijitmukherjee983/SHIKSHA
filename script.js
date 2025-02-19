affbbd95e7b24ce5a6a564b42ea58a4e50380169
Arijit Mukherjee <143013202+arijitmukherjee983@users.noreply.github.com>
2025-02-19 23:06:24 +0530

    changes

---
+++ script.js
@@ -0,0 +1,18 @@
+//function getStarted() {
+//    alert("Thank you for your interest! Please contact us for a demo.");
+//}
+
+function submitEmail() {
+    let email = document.getElementById("email").value;
+    if (email.includes("@")) {
+        alert("Thank you! We will contact you soon.");
+    } else {
+        alert("Please enter a valid email.");
+    }
+}
+function getStarted() {
+    window.open('https://debajitgithub.github.io/SHIKSHA_demo', '_blank'); // Replace with your actual URL
+}
+
+
+
